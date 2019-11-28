24ffbca596b8d34924a742d0626e0609464d82ad
lxhcool <1138659665@qq.com>
2019-11-28 18:07:16 +0800

    update

--- pages/player/index.js
+++ pages/player/index.js
@@ -5,8 +5,9 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-		bg: 'http://p2.music.126.net/0QKeN1ZJtrwCDoX8fK72CA==/109951164209431473.jpg',
-		title: '芙蓉雨'
+		bg: 'http://p1.music.126.net/678XIIRmFk1IGuv9RB43HQ==/109951163131639447.jpg',
+		title: '芙蓉雨',
+		isLyric: false,
 	},
 
 	/**
@@ -30,38 +31,9 @@ Page({
 
 	},
 
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
+	tabLyric () {
+		this.setData({
+			isLyric: !this.data.isLyric
+		})
 	}
 })
